95e01062f7d4c687cc32c8e1350236cd7ff74fe4
KokYenZein <kokyenzein@gmail.com>
2024-03-30 14:46:53 -0400

    working text_detection.py

--- text_detection.py
+++ text_detection.py
@@ -1,12 +1,19 @@
 import io
 import os
 from google.cloud import vision
+from google.oauth2 import service_account
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 def detect_text():
     """Detects text in the file."""
-    client = vision.ImageAnnotatorClient()
+    credentials_path = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
+    credentials = service_account.Credentials.from_service_account_file(credentials_path)
+    client = vision.ImageAnnotatorClient(credentials=credentials)
 
-    file = os.path.abspath("resources/image_test.jpg")
+    file = os.path.abspath(r"test_images\test1.jpg")
 
     with io.open(file, "rb") as image_file:
         content = image_file.read()
