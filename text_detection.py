80efc0beacb3a696d294dfe67fd366665fa889a2
KokYenZein <kokyenzein@gmail.com>
2024-03-30 12:45:33 -0400

    modified:   .gitignore new file:   image/fooo.txt new file:   text_detection.py

---
+++ text_detection.py
@@ -0,0 +1,35 @@
+import io
+import os
+from google.cloud import vision
+
+def detect_text():
+    """Detects text in the file."""
+    client = vision.ImageAnnotatorClient()
+
+    file = os.path.abspath("resources/image.jpg")
+
+    with io.open(file, "rb") as image_file:
+        content = image_file.read()
+
+    image = vision.Image(content=content)
+
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    print("Texts:")
+
+    for text in texts:
+        print(f'\n"{text.description}"')
+
+        vertices = [
+            f"({vertex.x},{vertex.y})" for vertex in text.bounding_poly.vertices
+        ]
+
+        print("bounds: {}".format(",".join(vertices)))
+
+    if response.error.message:
+        raise Exception(
+            "{}\nFor more info on error messages, check: "
+            "https://cloud.google.com/apis/design/errors".format(response.error.message)
+        )
+    
+    return(texts[0].description)
