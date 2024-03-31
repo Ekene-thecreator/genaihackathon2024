983600ab8111f5fcd5abc623c1da9cefbf7d0ad3
KokYenZein <kokyenzein@gmail.com>
2024-03-30 21:54:26 -0400

    prompt generation

--- query.py
+++ query.py
@@ -7,12 +7,9 @@ from google.oauth2 import service_account
 from vertexai import generative_models
 from vertexai.generative_models import GenerativeModel
 
-
 import os
 
-
 load_dotenv()
-
 
 credentials_path = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
 credentials = service_account.Credentials.from_service_account_file(os.path.expanduser(credentials_path))
--- rating.py
+++ rating.py
@@ -3,13 +3,26 @@ import os
 from requests.auth import HTTPBasicAuth
 from text_detection import detect_text
 from dotenv import load_dotenv
-from query import generate_answer
+import google.generativeai as genai
+from google.oauth2 import service_account
+from dotenv import load_dotenv
 
-dotenv_path = '/absolute/path/to/your/.env'
-load_dotenv(dotenv_path=dotenv_path)
+from google.cloud import aiplatform
+from google.oauth2 import service_account
+from vertexai import generative_models
+from vertexai.generative_models import GenerativeModel
+
+load_dotenv()
 
 TWILIO_ACCOUNT_SID = os.getenv('TWILIO_ACCOUNT_SID')
 TWILIO_AUTH_TOKEN = os.getenv('TWILIO_AUTH_TOKEN')
+
+credentials_path = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
+credentials = service_account.Credentials.from_service_account_file(os.path.expanduser(credentials_path))
+
+aiplatform.init(project="genai-hackathon-2024-418816", credentials=credentials)
+
+model = GenerativeModel("gemini-1.0-pro")
 
 def get_text_from_image(url):
     img_data = requests.get(url, auth=HTTPBasicAuth(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)).content
@@ -19,5 +32,9 @@ def get_text_from_image(url):
 
 def rating_prompt(url):
     text_from_image = get_text_from_image(url)
-    print(text_from_image)
-    return text_from_image
+    prompt = "Evaluate the safety level of the product below out of 100, based on its ingredients. \
+    List the top 5 ingredients with potential safety implications, ordered from most to least significant, \
+    Product description: \n" + text_from_image
+    response = model.generate_content(prompt)
+    print(response.text)
+    return response.text
