c582b7d0979e32939b9c404b65e08e35a562b958
Ekene Akuneme <jideoforekene42@gmail.com>
2024-05-28 10:24:56 -0400

    Cleaned up a bit

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, send_from_directory
-from twilio.twiml.messaging_response import MessagingResponse
 from google.cloud import texttospeech
 from rating import rating_prompt
 import os
@@ -7,57 +6,20 @@ import os
 # Initialize the Flask application
 app = Flask(__name__)
 
-# Retrieve and set the path for Google Cloud credentials from environment variables
-credentials_path = os.path.expanduser(os.getenv('GOOGLE_APPLICATION_CREDENTIALS'))
-
-# Initialize the Google Cloud Text-to-Speech client with credentials
-client = texttospeech.TextToSpeechClient.from_service_account_json(credentials_path)
-
 # Check if the 'resources' directory exists, create it if not
 resources_dir = 'resources'
 if not os.path.exists(resources_dir):
     os.makedirs(resources_dir)
 
-# Function to convert text to audio using Google Text-to-Speech
-def text_to_audio(text):
-    # Configure the text input, voice parameters, and audio configuration
-    synthesis_input = texttospeech.SynthesisInput(text=text)
-    voice = texttospeech.VoiceSelectionParams(language_code="en-US", ssml_gender=texttospeech.SsmlVoiceGender.FEMALE)
-    audio_config = texttospeech.AudioConfig(audio_encoding=texttospeech.AudioEncoding.MP3)
-
-    # Generate the speech synthesis response
-    response = client.synthesize_speech(input=synthesis_input, 
-                                        voice=voice, 
-                                        audio_config=audio_config)
-
-    # Save the audio content to an MP3 file in the 'resources' directory
-    with open("resources/output.mp3", "wb") as out:
-        out.write(response.audio_content)
-        print("Audio content written to file output.mp3")
-
 # Define the route for incoming data (e.g., from Twilio SMS)
 @app.route("/", methods=['GET', 'POST'])
 def incoming_data():
-    response = MessagingResponse()
-    message = response.message()
-    user_input = request.form.get("NumMedia")
+        picture_path = "test_images/test1.jpg"
+        rating = rating_prompt(picture_path)
 
-    # If there's at least one media item, process it
-    if user_input == "1":
-        picture_url = request.form.get("MediaUrl0")
-        rating = rating_prompt(picture_url)
-        text_to_audio(rating)
-        
-        # Append the rating text and the audio file URL to the response message
-        message.body("Rating & Description")
-        message.body(f"{rating}")
-        message.media("resources/output.mp3")
-        return str(response), 200
-    else:
-        # If no image is sent, ask the user to send one
-        return "Please send a picture containing ingredients", 400
+        return rating
 
-# Serve files from the 'resources' directory under the '/resources' path
+# Save files from the 'resources' directory under the '/resources' path
 @app.route("/resources/<path:path>")
 def send_resources(path):
     return send_from_directory("resources", path)
--- rating.py
+++ rating.py
@@ -12,44 +12,14 @@ from vertexai.generative_models import GenerativeModel
 
 from GeminiAPI import generate_response
 
-load_dotenv()
+def get_text_from_image(path):
+   
+   return f"image in {path} received!\n"
 
-# Retrieve Twilio credentials from environment variables
-TWILIO_ACCOUNT_SID = os.getenv('TWILIO_ACCOUNT_SID')
-TWILIO_AUTH_TOKEN = os.getenv('TWILIO_AUTH_TOKEN')
-
-# Retrieve Google Cloud credentials path from environment variables
-credentials_path = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
-credentials = service_account.Credentials.from_service_account_file(os.path.expanduser(credentials_path))
-
-aiplatform.init(project="genai-hackathon-2024-418816", credentials=credentials)
-
-# Initialize the generative model using Vertex AI with the specified model name
-model = GenerativeModel("gemini-1.0-pro")
-
-def get_text_from_image(url):
-    """Download an image from the provided URL using Twilio credentials and detect text within it."""
-    # Download the image content using HTTP GET request with Basic Auth for Twilio access
-    img_data = requests.get(url, auth=HTTPBasicAuth(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)).content
-    # Save the downloaded image to a file
-    with open('resources/image.jpg', 'wb') as handler:
-        handler.write(img_data)
-    
-    # Return the detected text from the image
-    return detect_text()
-
-def rating_prompt(url):
+def rating_prompt(path):
     """Generate a safety rating and description for a product based on its image URL."""
     # Extract text from the image at the given URL
-    text_from_image = get_text_from_image(url)
+    text_from_image = get_text_from_image(path)
 
-    prompt = "I have a product that I want to consume, below is the nutritional information or ingredients used. Please let me know how safe this product is for consumption. Please note that the nutritional information could be in any language however respond in English. Lastly, please limit your response to a 100 words. Nutritional Information: \n" + text_from_image
-    
-    # Generate a response based on the prompt
-    response = model.generate_content(prompt)
-    # response = generate_response(prompt, text_from_image)
-    
-    response_without_asterisks = response.text.replace("*", "")
-    # Print and return the cleaned response
-    print(response_without_asterisks)
-    return response_without_asterisks
+    print(text_from_image)
+    return text_from_image
